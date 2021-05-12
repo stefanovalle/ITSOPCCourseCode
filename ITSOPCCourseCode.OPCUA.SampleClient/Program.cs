033216dba4af5d1e492cfde2ca4edbd5b6a1bef8
Stefano Valle <s.valle@mvlabs.it>
2021-05-12 16:28:03 +0200

    Add empty SampleClient project

---
+++ ITSOPCCourseCode.OPCUA.SampleClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITSOPCCourseCode.OPCUA.SampleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
